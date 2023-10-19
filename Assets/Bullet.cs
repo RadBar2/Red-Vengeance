f6b2e906144e9759d61ed04d0062621b00673654
RadBar2 <rad.bartosevic@gmail.com>
2023-10-19 17:26:14 +0300

    a
    a

---
+++ Assets/Bullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 20;
+    public float lifetime = 3;
+    // Update is called once per frame
+
+    private void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool player1 = true;
+
+    [Header("Movement")]
+    public float speed = 10;
+
+    [Header("Shooting")]
+    public GameObject Bullet;
+    public float fireRate = 1;
+    public Transform firePoint;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(Shoot), fireRate, fireRate);
+    }
+
+    void Update()
+    {
+        var direction = new Vector3();
+        if(player1)
+        { 
+            direction.x = Input.GetAxis("Horizontal");
+            direction.z = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            direction.x = Input.GetAxis("Horizontal2");
+            direction.z = Input.GetAxis("Vertical2");
+        }
+
+
+        transform.position += direction * speed * Time.deltaTime;
+
+        if(direction != Vector3.zero)
+            transform.forward = direction;
+    }
+
+    void Shoot()
+    {
+        Instantiate(Bullet, firePoint.position, transform.rotation);
+    }
+}
